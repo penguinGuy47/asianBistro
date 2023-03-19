4e57d48dc5ed93259209bb57163bde1c5456b02e
Kaleb <kaleblianggg@icloud.com>
2023-03-19 02:05:40 -0500

    address-phone bar, horizontal scroll, fb, gallery, about

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav ul li a');
-
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("nav ul li a");
+  
     navLinks.forEach((link) => {
-        link.addEventListener('mouseenter', () => {
-            link.style.transform = 'scale(1.1)';
-            link.style.transition = 'transform 0.3s';
-        });
-
-        link.addEventListener('mouseleave', () => {
-            link.style.transform = 'scale(1)';
-            link.style.transition = 'transform 0.3s';
-        });
+      link.addEventListener("mouseenter", () => {
+        link.style.transform = "scale(1.1)";
+        link.style.transition = "transform 0.3s";
+      });
+  
+      link.addEventListener("mouseleave", () => {
+        link.style.transform = "scale(1)";
+        link.style.transition = "transform 0.3s";
+      });
     });
-
-    const photoCards = document.querySelectorAll('.photo-card');
-    photoCards.forEach((card) => {
-        card.addEventListener('mouseenter', () => {
-            card.style.transform = 'rotateY(180deg)';
-            card.style.transition = 'transform 0.5s';
-        });
-
-        card.addEventListener('mouseleave', () => {
-            card.style.transform = 'rotateY(0deg)';
-            card.style.transition = 'transform 0.5s';
-        });
-    });
+  });
+  
+  let lastScrollTop = 0;
+  
+  function manageHeaderVisibility() {
+    const currentScrollTop = window.pageYOffset;
+    const topBar = document.getElementById("top-bar");
+    const header = document.querySelector("header");
+    const homeSection = document.querySelector("#home");
+    const homeSectionBottom = homeSection.offsetTop + homeSection.offsetHeight;
+  
+    if (currentScrollTop > lastScrollTop && currentScrollTop >= homeSectionBottom) {
+        topBar.style.display = "none";
+        header.style.transform = "translateY(-100%)";
+        header.style.transition = "transform 0.3s";
+    } else {
+        topBar.style.display = "flex";
+        header.style.transform = "translateY(0)";
+        header.style.transition = "transform 0.3s";
+    }
+  
+    const menuSection = document.querySelector("#menu");
+    const menuSectionTop = menuSection.offsetTop;
+  
+    if (currentScrollTop >= menuSectionTop) {
+        if (!header.classList.contains("sticky")) {
+            header.classList.add("sticky");
+            header.style.transform = "translateY(0)";
+            header.style.transition = "transform 0.3s";
+        }
+    } else {
+        header.classList.remove("sticky");
+    }
     
-});
+  
+    lastScrollTop = currentScrollTop;
+  }
+  
+  window.addEventListener("scroll", manageHeaderVisibility);
+  
